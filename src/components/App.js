77aaee188378207e5bcddd1e69232aa6cabe15cd
jayasankar123 <jayasankarreddy0@gmail.com>
2020-11-01 18:01:50 +0530

    todolistWORKING

--- src/components/App.js
+++ src/components/App.js
@@ -1,29 +1,79 @@
 import React from "react";
 import "./../styles/App.css";
-import ToDoList from "./ToDoList";
-let tasksArray = [];
+//import ToDoList from "./ToDoList";
+
+//let index = -1;
 function App() {
   let [tasks, updateTasks] = React.useState([]);
+  let tasksArr = [...tasks];
   let [textArea, updateTextArea] = React.useState("");
+  let str = "";
+
+  function handleTaskChange(event) {
+    str = event.target.value;
+    return;
+  }
+
+  function saveTask(index) {
+    if (str === "") {
+      return;
+    }
+    let obj = { ...tasksArr[index] };
+    obj.edit = false;
+    obj.content = str;
+    tasksArr[index] = { ...obj };
+    updateTasks([...tasksArr]);
+    str = "";
+    return;
+  }
 
   function handleChange(event) {
     updateTextArea(event.target.value);
+    //console.log(textArea);
+    return;
   }
+
   function handleClick() {
-    if (textArea.trim() === "") {
+    let textContent = textArea;
+    if (textContent.trim() === "") {
       return;
-    } else {
-      tasksArray.push(textArea);
-      updateTasks(tasksArray);
-      console.log(tasks);
-      updateTextArea("");
     }
+    let obj = {};
+    obj.content = textArea;
+    obj.edit = false;
+    tasksArr.push(obj);
+    updateTasks([...tasksArr]);
+    updateTextArea("");
   }
-  function handleDelete(event, content) {
-    tasksArray = tasksArray.filter((el) => {
-      if (el !== content) return el;
+
+  function handleDelete(index) {
+    tasksArr = tasksArr.filter((el, i) => {
+      return i !== index;
     });
-    updateTasks(tasksArray);
+    updateTasks([...tasksArr]);
+  }
+
+  function handleEdit(index) {
+    if (tasksArr[index].edit === true) {
+      return;
+    }
+    let obj = {};
+    let element = (
+      <>
+        <textarea
+          className="edit task"
+          onChange={() => handleTaskChange(event)}
+        ></textarea>
+        <button className="save task" onClick={() => saveTask(index)}>
+          SAVE TASK
+        </button>
+      </>
+    );
+    obj.content = tasksArr[index].content;
+    obj.edit = true;
+    obj.element = element;
+    tasksArr[index] = { ...obj };
+    updateTasks([...tasksArr]);
   }
 
   return (
@@ -32,13 +82,27 @@ function App() {
         //Do not alter main div
         //Please do not alter the functional component as tests depend on the type of component.
       }
-      <form>
-        <textarea id="task" onChange={handleChange} value={textArea}></textarea>
-      </form>
-      <button onClick={handleClick}>Add Task</button>
-      {tasksArray.map((el) => (
-        <ToDoList content={el} del={handleDelete} />
-      ))}
+      <textarea id="task" onChange={handleChange} value={textArea}></textarea>
+      <button id="btn" onClick={handleClick}>
+        ADD TASKS
+      </button>
+      <ul>
+        {tasks.map((el, index) => (
+          <li key={el.content} className="list">
+            {el.content}
+            {el.edit && el.element}
+            <div>
+              <button className="edit" onClick={() => handleEdit(index)}>
+                EDIT
+              </button>
+
+              <button className="delete" onClick={() => handleDelete(index)}>
+                DELETE
+              </button>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,13 +1,8 @@
 import React from "react";
 
 export default function ToDoList({ content, del }) {
-  return (
-    <>
-      <form>
-        <textarea value={content}></textarea>
-      </form>
-      <button>edit</button>
-      <button onClick={del(content)}>delete</button>
-    </>
-  );
+  function handleDelete(delContent) {}
+  function handleEdit(editContent, indexel) {}
+  return <></>;
 }
+//onClick={del(content)}
